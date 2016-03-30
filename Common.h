a98b4a02246ce42987152d053873acd1195d01f0
Menno Buggenhout <mennobuggenhout@ptr-2hj4tbikqw47c3cglxls7vssc.ip6.access.telenet.be>
2016-03-30 19:36:48 +0200

    Initial Commit

---
+++ Common.h
@@ -0,0 +1,18 @@
+//
+//  Common.h
+//  Dunjun
+//
+//  Created by Menno Buggenhout on 2/03/16.
+//  Copyright © 2016 Menno Buggenhout. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+namespace Dunjun
+{
+using Float32 = float;
+using Float64 = double;
+}//namespace Dunjun
+
+#endif /* Common_h */
--- Dunjun/main.cpp
+++ Dunjun/main.cpp
@@ -6,10 +6,17 @@
 //  Copyright © 2016 Menno Buggenhout. All rights reserved.
 //
 
+#include "../Common.h"
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    
+    std::cout << "Hellope!\n";
+    
+    std::cin.ignore();
+    
     return 0;
+    
 }
